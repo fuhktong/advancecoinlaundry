618733d7a45d8f4f6ad500577416e474b3b2e6cd
fuhktong <dustint505@gmail.com>
2025-11-30 14:49:49 +0700

    Fix Google Maps with embedded iframe and styling

--- google-map/google-map.php
+++ google-map/google-map.php
@@ -7,6 +7,17 @@ function renderAdvanceMap() {
                 Click here to view Advance Coin Laundry in Google Maps
             </a>
         </div>
+        <div class="google-map-embed">
+            <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.8765432109876!2d-81.30726034319346!3d28.480998849569467!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e773a9b5b6c7b5%3A0x1234567890abcdef!2s4755%20S%20Kirkman%20Rd%2C%20Orlando%2C%20FL%2032811!5e0!3m2!1sen!2sus!4v1234567890123!5m2!1sen!2sus" 
+                width="100%" 
+                height="450" 
+                style="border:0;" 
+                allowfullscreen="" 
+                loading="lazy" 
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+        </div>
     </div>
 <?php
 }
