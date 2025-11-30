d50a287f3125755b5e4f479eb92fbed0c8000bc9
fuhktong <dustint505@gmail.com>
2025-11-30 14:48:01 +0700

    Add GitHub Actions deployment

--- google-map/google-map.php
+++ google-map/google-map.php
@@ -1,7 +1,5 @@
 <?php
 function renderAdvanceMap() {
-    // You'll need to replace 'YOUR_GOOGLE_MAPS_API_KEY' with your actual API key
-    $googleMapsApiKey = 'YOUR_GOOGLE_MAPS_API_KEY';
 ?>
     <div class="map-container">
         <div class="map-link">
@@ -9,26 +7,7 @@ function renderAdvanceMap() {
                 Click here to view Advance Coin Laundry in Google Maps
             </a>
         </div>
-        <div id="googleMap" style="width: 90%; height: 600px; margin: 0 auto;"></div>
     </div>
-
-    <script>
-        function initMap() {
-            const location = { lat: 28.480998849569467, lng: -81.30726034319346 };
-            
-            const map = new google.maps.Map(document.getElementById("googleMap"), {
-                zoom: 12,
-                center: location,
-            });
-            
-            const marker = new google.maps.Marker({
-                position: location,
-                map: map,
-                title: "Advance Coin Laundry"
-            });
-        }
-    </script>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=<?php echo $googleMapsApiKey; ?>&callback=initMap"></script>
 <?php
 }
 ?>
