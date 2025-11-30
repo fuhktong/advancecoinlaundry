3236f8194ab225ab676c9a11489c88a4a47330ef
fuhktong <dustint505@gmail.com>
2025-11-30 15:11:18 +0700

    Fix reviews page - correct include paths for google-reviews

--- pages/reviews.php
+++ pages/reviews.php
@@ -1,11 +1,11 @@
 <?php
 include 'header/header.php';
-include 'googlereviews/googlereviews.php';
+include 'google-reviews/google-reviews.php';
 include 'footer-contact/footer-contact.php';
 ?>
 
 <link rel="stylesheet" href="/sections/sections.css">
-<link rel="stylesheet" href="/googlereviews/googlereviews.css">
+<link rel="stylesheet" href="/google-reviews/google-reviews.css">
 <link rel="stylesheet" href="/footer-contact/footer-contact.css">
 
 <?php renderGoogleReviews(); ?>
