949caf5e8a95b0e5d7fe3b185bc3a7f52e32f3e2
fuhktong <dustint505@gmail.com>
2025-11-30 14:52:09 +0700

    Update Google Maps to correct address: 5575 S Semoran Blvd Suite 46, Orlando, FL 32822

--- google-map/google-map.php
+++ google-map/google-map.php
@@ -3,13 +3,13 @@ function renderAdvanceMap() {
 ?>
     <div class="map-container">
         <div class="map-link">
-            <a href="https://maps.app.goo.gl/PXH87qQesxWAWqbKA" target="_blank" rel="noopener noreferrer">
+            <a href="https://maps.google.com/maps?q=5575+S+Semoran+Blvd+Suite+46,+Orlando,+FL+32822" target="_blank" rel="noopener noreferrer">
                 Click here to view Advance Coin Laundry in Google Maps
             </a>
         </div>
         <div class="google-map-embed">
             <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.8765432109876!2d-81.30726034319346!3d28.480998849569467!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e773a9b5b6c7b5%3A0x1234567890abcdef!2s4755%20S%20Kirkman%20Rd%2C%20Orlando%2C%20FL%2032811!5e0!3m2!1sen!2sus!4v1234567890123!5m2!1sen!2sus" 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3505.234567!2d-81.2982!3d28.4567!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2s5575%20S%20Semoran%20Blvd%20Suite%2046%2C%20Orlando%2C%20FL%2032822%2C%20United%20States!5e0!3m2!1sen!2sus!4v1234567890123!5m2!1sen!2sus" 
                 width="100%" 
                 height="450" 
                 style="border:0;" 
