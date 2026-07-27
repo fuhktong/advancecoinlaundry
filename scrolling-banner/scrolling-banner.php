5247eff4cca0e0513a9e7f1321c91203645c4d39
fuhktong <dustint505@gmail.com>
2026-07-27 20:23:48 +0700

    Manual deployment

--- scrolling-banner/scrolling-banner.php
+++ scrolling-banner/scrolling-banner.php
@@ -1,10 +1,10 @@
 <?php
 function renderScrollingBanner() {
 ?>
-    <div class="announcement-banner">
+    <!-- <div class="announcement-banner">
         We will be closed Thursday, July 3rd at 4PM for an Employee </br>
         Appreciation Event. Thank you for your understanding!
-    </div>
+    </div> -->
     <div class="scrolling-banner">
         <div class="banner-content">
             <a href="https://maps.app.goo.gl/SGT55qugeUi77bAr5" 
